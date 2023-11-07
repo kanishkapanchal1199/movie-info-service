be6b66ca1ff4369c425cc3f944b58d3b8855d32d
kanishkapanchal1199 <kanishkajpanchal@outlook.com>
2023-11-07 20:28:27 +0530

    Initial commit

---
+++ src/main/java/com/learn/microservices/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,25 @@
+package com.learn.microservices.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MovieInfoServiceApplication {
+	
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
